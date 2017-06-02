c81e6cf1e1226df456c2c1fa11c34fb3f980fa52
seescode <seesyong@gmail.com>
2017-06-02 00:16:47 -0400

    implement budget list on left hand nav

--- src/app/containers/budgeting-page/budgeting-page-left-nav/budgeting-page-left-nav.component.ts
+++ src/app/containers/budgeting-page/budgeting-page-left-nav/budgeting-page-left-nav.component.ts
@@ -1,3 +1,9 @@
+import { budgetSelector } from './../../../selectors/selectors';
+import { Router } from '@angular/router';
+import { AppState } from './../../../reducers/index';
+import { Store } from '@ngrx/store';
+import { Budget } from './../../../models/interfaces';
+import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +13,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BudgetingPageLeftNavComponent implements OnInit {
 
-  constructor() { }
+  budgets$: Observable<Budget[]>;
+
+  constructor(private store: Store<AppState>, private router: Router) { }
 
   ngOnInit() {
+    this.budgets$ = this.store.select(budgetSelector);
   }
 
+  openBudget(budgetId: string) {
+    const month = new Date().getMonth();
+    const year = new Date().getFullYear();
+
+    this.router.navigate(['/budgeting', budgetId, year, month + 1]);
+  }
 }
